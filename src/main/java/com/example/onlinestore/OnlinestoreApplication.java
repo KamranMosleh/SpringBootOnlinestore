98271dd43ef5522593f184a3efe3cfa3d286b3ee
Kamran Mosleh Khorrami <139634188+KamranMosleh@users.noreply.github.com>
2025-04-13 00:51:11 +0200

    commit after after adding the Autowired annotation and additional constructor

--- src/main/java/com/example/onlinestore/OnlinestoreApplication.java
+++ src/main/java/com/example/onlinestore/OnlinestoreApplication.java
@@ -2,14 +2,16 @@ package com.example.onlinestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class OnlinestoreApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(OnlinestoreApplication.class, args);
-		var orderService = new OrderService();
-		orderService.setPaymentService(new PayPalPaymentService());
+		ApplicationContext context = SpringApplication.run(OnlinestoreApplication.class, args);
+		var orderService = context.getBean(OrderService.class);
+//		var orderService = new OrderService();
+//		orderService.setPaymentService(new PayPalPaymentService());
 		orderService.placeOrder();
 	}
 }
--- src/main/java/com/example/onlinestore/OrderService.java
+++ src/main/java/com/example/onlinestore/OrderService.java
@@ -1,11 +1,20 @@
+
 package com.example.onlinestore;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
-//    public OrderService(PaymentService paymentService) {
-//        this.paymentService = paymentService;
-//    }
+    public OrderService(PaymentService paymentService, int x) {}
+
+    @Autowired
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder(){
         // use a payment service here like Stripe
